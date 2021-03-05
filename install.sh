202db84f92d22b22f8f2edb41a69897c0785516d
Jai-JAP <parjailu@gmail.com>
2021-03-05 16:49:27 +0530

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Cloning the Updated neofetch script from https://raw.githubusercontent.com/Jai-JAP/neofetch/main/neofetch to /tmp/neofetch
+wget -q https://raw.githubusercontent.com/Jai-JAP/neofetch/main/neofetch -O /tmp/neofetch
+
+# Make neofetch script executable
+chmod +x /tmp/neofetch
+
+# Moving the neofetch script to /usr/bin
+sudo mv /tmp/neofetch /usr/bin/neofetch
+
+# Removing all old neofetch configs
+sudo rm -rf ~/.config/neofetch/*
+
+# Removing the neofetch folder from /tmp
+sudo rm /tmp/neofetch
+
+# Installing dependencies required to improve .neofetch script
+sudo apt install wmctrl
+
+# Updating the .neofetch script in home directory
+sed -i 's/neofetch/wmctrl -i -r $WINDOWID -b toggle,maximized_vert,maximized_horz\nneofetch/g' ~/.neofetch 
